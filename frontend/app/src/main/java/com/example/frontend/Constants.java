d80ca8e5f39ca1e869e6e93596c1d2d21ecaf53d
Wilson <wilsontestonijr@gmail.com>
2024-05-17 00:29:19 -0300

    Ajustando o layout da barra de navegação e o curso expandido

--- frontend/app/src/main/java/com/example/frontend/Constants.java
+++ frontend/app/src/main/java/com/example/frontend/Constants.java
@@ -3,6 +3,6 @@ package com.example.frontend;
 /** Classe que armazena constantes usadas em todo o aplicativo. */
 public class Constants {
     /** URL base para a API ou serviço web */
-    public static final String BASE_URL = "http://192.168.0.10:4550";
+    public static final String BASE_URL = "http://192.168.0.10:4551";
     public static String userName = "Testando nome";
 }
